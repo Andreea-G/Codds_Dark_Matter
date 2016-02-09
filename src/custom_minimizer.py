Small bug fixed in multi experiment constrained likelihood calculation

src/custom_minimizer.py
@@ -35,139 +35,139 @@ __all__ = ['Custom_SelfConsistent_Minimization']
 
 
 """
-The general idea here is this: 
+The general idea here is this:
 Can I create a minimization procedure that is more efficient than that standard basinhopping (and finds the actual minimium).
-To do this, I plan on picking my value of vmin, minimizing wrt to the value of eta, then basinhopping at a fixed 
-eta across different vmin, then reminimize wrt to eta, etc. 
+To do this, I plan on picking my value of vmin, minimizing wrt to the value of eta, then basinhopping at a fixed
+eta across different vmin, then reminimize wrt to eta, etc.
 
-Do this procedure until a self consistent minimum is found. I will start by writing this code for CDMS-Si and 1 other 
+Do this procedure until a self consistent minimum is found. I will start by writing this code for CDMS-Si and 1 other
 poisson binned experiment, but I'd like to generalize at some point in the future.
 
-TODO Generalize st more than 2 experiments can be used
-"""  
+TODO Generalize to: more than 2 experiments, different likelihood functions, larger N_o -- also clean up and write neater code
+"""
 
 
 
 class CustomMinimize():
     def __init__(self, class_names):
         self.class_names = class_names
-       
+
     def Custom_SelfConsistent_Minimization(class_name, x0, mx, fp, fn, delta,
-                                           bh_iter = 15, vmin_err = 7.0, logeta_err = 0.05,
+                                           bh_iter = 15, vmin_err = 7.0, logeta_err = 0.01,
                                            vmin_step = 15.0):
-    
-        
+
+
         vmin_list = x0[:3]
         logeta_list = x0[3:]
         vmin_listw0 = np.insert(vmin_list, 0, 0)
         rate_partials = [None] * (class_name[1].BinEdges_left.size)
-    
+
         script_N_a = class_name[0].IntegratedResponseTable(vmin_listw0)
         script_M_a = class_name[0].VminIntegratedResponseTable(vmin_listw0)
-     
-    
+
+
         for x in range(0, class_name[1].BinEdges_left.size):
             resp_integr = class_name[1].IntegratedResponseTable(vmin_listw0,
                       class_name[1].BinEdges_left[x], class_name[1].BinEdges_right[x], mx, fp, fn, delta)
-                      
+
             rate_partials[x] = resp_integr
-        
+
         def constr_func_logeta(x):
 
             constraints = np.concatenate([-x, np.diff(-x)])
-            
+
             is_not_close = np.logical_not(
                 np.isclose(constraints, np.zeros_like(constraints), atol=1e-3))
             is_not_close[x.size] = True
             constr = np.where(is_not_close, constraints, np.abs(constraints))
-            
+
 #            print("***constr =", repr(constr))
 #            print("tf =", repr(constr < 0))
-            
+
             return constr
-            
+
         def constr_func_vmin(x):
 
             constraints = np.concatenate([x, np.diff(x)])
-            
+
             is_not_close = np.logical_not(
                 np.isclose(constraints, np.zeros_like(constraints), atol=1e-3))
             is_not_close[x.size] = True
             constr = np.where(is_not_close, constraints, np.abs(constraints))
-            
+
 #            print("***constr =", repr(constr))
 #            print("tf =", repr(constr < 0))
-            
+
             return constr
-            
+
         constr = ({'type': 'ineq', 'fun': constr_func_logeta})
         constr_vmin = ({'type': 'ineq', 'fun': constr_func_vmin})
-        
+
         def optimize_logeta(logeta_list, class_name, script_N_a, script_M_a,
                     rate_partials):
-    
-            optimize_func = (2.0 * (class_name[0].NBKG + class_name[0].Exposure * np.dot(10**logeta_list, script_N_a) - 
+
+            optimize_func = (2.0 * (class_name[0].NBKG + class_name[0].Exposure * np.dot(10**logeta_list, script_N_a) -
                 np.log(class_name[0].mu_BKG_i + class_name[0].Exposure * np.dot(script_M_a, 10**logeta_list)).sum()))
-                
+
             for x in range(0, class_name[1].BinData.size):
-                
+
                 if (class_name[1].Exposure * np.dot(10**logeta_list, rate_partials[x])) > class_name[1].BinData[x]:
                     optimize_func += 2.0 * (class_name[1].Exposure * np.dot(10**logeta_list, rate_partials[x]) + log(factorial(class_name[1].BinData[x])) -
                                  class_name[1].BinData[x] * log(class_name[1].Exposure * np.dot(10**logeta_list, rate_partials[x])))
                 elif class_name[1].BinData[x] > (class_name[1].Exposure * np.dot(10**logeta_list, rate_partials[x])):
                         optimize_func += 2.0 * (class_name[1].BinData[x] + log(factorial(class_name[1].BinData[x])) -
                                        class_name[1].BinData[x] * log(class_name[1].BinData[x]))
-                         
+
             return optimize_func
 
         def minimize_over_vmin(vmin_list, logeta_list, class_name, mx, fp, fn, delta):
-        
+
             vmin_listw0 = np.insert(vmin_list, 0, 0)
             rate_partials = [None] * (class_name[1].BinEdges_left.size)
-    
+
             script_N_a = class_name[0].IntegratedResponseTable(vmin_listw0)
             script_M_a = class_name[0].VminIntegratedResponseTable(vmin_listw0)
 
-            optimize_func = (2.0 * (class_name[0].NBKG + class_name[0].Exposure * np.dot(10**logeta_list, script_N_a) - 
+            optimize_func = (2.0 * (class_name[0].NBKG + class_name[0].Exposure * np.dot(10**logeta_list, script_N_a) -
                 np.log(class_name[0].mu_BKG_i + class_name[0].Exposure * np.dot(script_M_a, 10**logeta_list)).sum()))
-                
+
             for x in range(0, class_name[1].BinEdges_left.size):
                 resp_integr = class_name[1].IntegratedResponseTable(vmin_listw0,
                           class_name[1].BinEdges_left[x], class_name[1].BinEdges_right[x], mx, fp, fn, delta)
                 rate_partials[x] = resp_integr
-              
+
                 if (class_name[1].Exposure * np.dot(10**logeta_list, rate_partials[x])) > class_name[1].BinData[x]:
                         optimize_func += 2.0 * (class_name[1].Exposure * np.dot(10**logeta_list, rate_partials[x]) + log(factorial(class_name[1].BinData[x])) -
                                  class_name[1].BinData[x] * log(class_name[1].Exposure * np.dot(10**logeta_list, rate_partials[x])))
                 elif class_name[1].BinData[x] > (class_name[1].Exposure * np.dot(10**logeta_list, rate_partials[x])):
                         optimize_func += 2.0 * (class_name[1].BinData[x] + log(factorial(class_name[1].BinData[x])) -
                                        class_name[1].BinData[x] * log(class_name[1].BinData[x]))
-                         
+
             return optimize_func
-        
+
         ni = 0
         check = False
 
-        
-        while ni < 15 and not check: 
-            mloglike_min = minimize(optimize_logeta, logeta_list, args=(class_name, script_N_a, script_M_a, 
-                                rate_partials), method = 'SLSQP', 
+        while ni < 15 and not check:
+            mloglike_min = minimize(optimize_logeta, logeta_list, args=(class_name, script_N_a, script_M_a,
+                                rate_partials), method = 'SLSQP',
                                 bounds = [(-35.0, -24.0),(-35.0, -24.0),(-35.0, -24.0)], constraints=constr)
             logeta_list_new = mloglike_min.x
-        
-            minimizer_kwargs = {"method": "SLSQP", "constraints": constr_vmin, 
+
+            minimizer_kwargs = {"method": "SLSQP", "constraints": constr_vmin,
                             "args": (logeta_list_new, class_name, mx, fp, fn, delta),
                             "options": {'ftol': 1.0}}
             vminloglike_min = basinhopping(minimize_over_vmin, vmin_list,
-                        minimizer_kwargs=minimizer_kwargs, niter=bh_iter, stepsize=vmin_step)   
-#            vminloglike_min = minimize(minimize_over_vmin, vmin_list, args=(logeta_list_new, class_name, mx, fp, fn, delta), method = 'SLSQP', 
-#                                bounds = [(300, 900),(300, 900),(300, 900)], constraints=constr_vmin, options={'ftol': 1.0})
+                        minimizer_kwargs=minimizer_kwargs, niter=bh_iter, stepsize=vmin_step)
+
             vmin_list_new = vminloglike_min.x
 
             vmin_check = np.abs(np.asarray(vmin_list_new) - np.asarray(vmin_list))
             logeta_check = np.abs(np.asarray(logeta_list_new) - np.asarray(logeta_list))
 
             if np.amax(vmin_check) < vmin_err and np.amax(logeta_check) < logeta_err:
+                print('n_iter', ni)
+                print('Minimum Found')
                 check = True
             else:
                 print('n_iter', ni)
@@ -180,9 +180,9 @@ class CustomMinimize():
                 vmin_list = vmin_list_new
                 logeta_list = logeta_list_new
             ni += 1
-        
+
         return np.concatenate([vmin_list, logeta_list])
 
 
 
-    
+

src/experiment_HaloIndep_Band.py
@@ -1026,6 +1026,7 @@ class Experiment_EHI(Experiment_HaloIndep):
         attempts = 3
         np.random.seed(1)
         random_variation = 1e-5
+        expernum = multiexper_input.size
 
         if USE_BASINHOPPING:
             class TakeStep(object):
@@ -1084,9 +1085,13 @@ class Experiment_EHI(Experiment_HaloIndep):
                         CustomMinimize.Custom_SelfConsistent_Minimization(class_name, vars_guess, mx, fp, fn, delta)
 #                        minimize(self.MultiExperimentMinusLogLikelihood, vars_guess,
 #                                 args=args, constraints=constr, method=self.method)
-                        
-                        likelihood_min = MultiExperimentMinusLogLikelihood(constr_optimum_log_likelihood, 
-                                        multiexper_input, class_name, mx, fp, fn, delta)
+                    
+                    likelihood_min = (self._MinusLogLikelihood(constr_optimum_log_likelihood, vminStar=vminStar,
+                                            logetaStar=logetaStar, vminStar_index=vminStar_index) +
+                                            sum([class_name[x]._MinusLogLikelihood(constr_optimum_log_likelihood,
+                                            mx, fp, fn, delta)
+                                            for x in range(1, expernum)]))
+                    
                     constraints = constr_func(constr_optimum_log_likelihood)
                 is_not_close = np.logical_not(np.isclose(constraints,
                                                          np.zeros_like(constraints)))
@@ -1219,14 +1224,14 @@ class Experiment_EHI(Experiment_HaloIndep):
 #                logetaStar_rand = constr_optimum_log_likelihood.minimizer.kwargs['args'][2]
                 constr_func = ConstraintsFunction(vminStar_rand, logetaStar_rand,
                                                   vminStar_index)
-                constraints = constr_func(constr_optimum_log_likelihood.[0])
+                constraints = constr_func(constr_optimum_log_likelihood[0])
                 is_not_close = np.logical_not(np.isclose(constraints,
                                                          np.zeros_like(constraints)))
                 constr_not_valid = np.logical_and(constraints < 0, is_not_close)
                 sol_not_found = np.any(constr_not_valid)
                 print("random vminStar =", vminStar_rand)
                 print("random logetaStar =", logetaStar_rand)
-                print("x =", constr_optimum_log_likelihood.[0])
+                print("x =", constr_optimum_log_likelihood[0])
                 print("constraints =", constraints)
                 print("is_not_close =", is_not_close)
                 print("constr_not_valid =", constr_not_valid)