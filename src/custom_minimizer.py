Fixed small issue with Optimal likelihood. Corrected Multiexperiment CB calculation. Added small feature that may allow bounds and multiexperiment CB to be calculated.

src/custom_minimizer.py
@@ -130,12 +130,13 @@ def Custom_SelfConsistent_Minimization(class_name, x0, mx, fp, fn, delta, vminSt
             optimize_func = (2.0 * (class_name[0].NBKG + class_name[0].Exposure * np.dot(10**logeta_list, script_N_a) +
                 log(factorial(len(class_name[0].ERecoilList))) - np.log(class_name[0].mu_BKG_i + class_name[0].Exposure *
                 np.dot(script_M_a, 10**logeta_list)).sum()))
+
 # Add in other likelihoods            
             for x in range(0, class_name[1].BinData.size):
                 optimize_func += 2.0 * (class_name[1].BinExposure[x] * np.dot(10**logeta_list, rate_partials[x]) + class_name[1].BinBkgr[x] +
                     log(factorial(class_name[1].BinData[x])) - class_name[1].BinData[x] * log(class_name[1].BinExposure[x] * 
                     np.dot(10**logeta_list, rate_partials[x]) + class_name[1].BinBkgr[x]))
-               
+
             return optimize_func
 
         def minimize_over_vmin(vmin_list, logeta_list, class_name, mx, fp, fn, delta, vminStar=None):
@@ -154,6 +155,7 @@ def Custom_SelfConsistent_Minimization(class_name, x0, mx, fp, fn, delta, vminSt
                 log(factorial(len(class_name[0].ERecoilList))) -
                 np.log(class_name[0].mu_BKG_i + class_name[0].Exposure * np.dot(script_M_a, 10**logeta_list)).sum()))
             
+            
             for x in range(0, class_name[1].BinEdges_left.size):
                 resp_integr = class_name[1].IntegratedResponseTable(vmin_listw0,
                           class_name[1].BinEdges_left[x], class_name[1].BinEdges_right[x], mx, fp, fn, delta)
@@ -162,7 +164,7 @@ def Custom_SelfConsistent_Minimization(class_name, x0, mx, fp, fn, delta, vminSt
                 optimize_func += 2.0 * (class_name[1].BinExposure[x] * np.dot(10**logeta_list, rate_partials[x]) + class_name[1].BinBkgr[x] +
                     log(factorial(class_name[1].BinData[x])) - class_name[1].BinData[x] * log(class_name[1].BinExposure[x] * 
                     np.dot(10**logeta_list, rate_partials[x]) + class_name[1].BinBkgr[x]))
-
+            
             return optimize_func
 
         ni = 0
@@ -188,6 +190,7 @@ def Custom_SelfConsistent_Minimization(class_name, x0, mx, fp, fn, delta, vminSt
                                 method = 'SLSQP', bounds = bnd_vmin, constraints=constr_vmin)
             vmin_list_new_reduced = vminloglike_min.x
             
+            
             if vminStar is not None:
                 vmin_list_new = np.sort(np.append(vmin_list_new_reduced, vminStar))
             else:
@@ -208,6 +211,7 @@ def Custom_SelfConsistent_Minimization(class_name, x0, mx, fp, fn, delta, vminSt
             if np.amax(vmin_check) < vmin_err and np.amax(logeta_check) < logeta_err:
                 print('n_iter', ni)
                 print('Minimum Found')
+                loglikeval = vminloglike_min.fun
                 check = True
             else:
                 print('n_iter', ni)
@@ -224,7 +228,7 @@ def Custom_SelfConsistent_Minimization(class_name, x0, mx, fp, fn, delta, vminSt
         if not check:
             print('Potential issue with convergence...')
 
-        return [np.concatenate([vmin_list_new, logeta_list_new]), vminloglike_min.fun]
+        return np.concatenate([vmin_list_new, logeta_list_new]), loglikeval
 
 
 

src/experiment_HaloIndep.py
@@ -432,7 +432,7 @@ class GaussianExperiment_HaloIndep(Experiment_HaloIndep):
 
         result = np.zeros((self.BinData.size, 1001))
         # TODO parallelize this section of the code
-        calculate_Q = True
+        calculate_Q = False
         if calculate_Q:
             for x in range(0, self.BinData.size):
                 for v_dummy in range(1, 1001):
@@ -603,7 +603,7 @@ class MultExper_Binned_exper_P(Experiment_HaloIndep):
                         result[x, v_dummy] = (2.0 * ((self.BinExposure[x] * rate_partials[x] + self.BinBkgr[x] - self.BinData[x]) /
                            ( rate_partials[x] + self.BinBkgr[x] / self.BinExposure[x])) * self.curly_H_tab[x, v_dummy])
                         self.Q_contrib[x, v_dummy] = result[x, v_dummy]
-                        print(self.Q_contrib[x, v_dummy])
+                        
             file = Output_file_name(self.name, self.scattering_type, self.mPhi, mx, fp, fn, delta,
                              F, "_KKT_Cond_1", "../Output_Band/") + ".dat"
             f_handle = open(file, 'wb')   # clear the file first
@@ -749,12 +749,17 @@ class Crosses_HaloIndep(Experiment_HaloIndep):
         mT_avg = np.sum(self.mT * self.mass_fraction) / np.sum(self.mass_fraction)
         print("mT_avg =", mT_avg)
         print('vmax =', vmax)
-        kwargs = ({'Eee1': Eee1, 'Eee2': Eee2, 'mT_avg': mT_avg,
+        arraybox = np.array([])
+        for Eee1, Eee2 in zip(self.BinEdges_left, self.BinEdges_right):
+            kwargs = {'Eee1': Eee1, 'Eee2': Eee2, 'mT_avg': mT_avg,
                    'mx': mx, 'fp': fp, 'fn': fn, 'delta': delta, 'vmax': vmax,
                    'output_file': output_file}
-                  for Eee1, Eee2 in zip(self.BinEdges_left, self.BinEdges_right))
-
-        return np.array(par.parmap(self._Box, kwargs, processes))
+            if Eee1 == self.BinEdges_left[0]:
+                arraybox = np.append(arraybox, self._Box(**kwargs))
+            else:
+                arraybox = np.column_stack((arraybox, np.array(self._Box(**kwargs))))
+            
+        return arraybox
 
     def _Rebin(self, index=9):
         self.BinEdges = np.append(self.BinEdges[:index + 1],  self.BinEdges[-1])
@@ -773,11 +778,11 @@ class Crosses_HaloIndep(Experiment_HaloIndep):
             self._Rebin()
 
         box_table = self._Boxes(mx, fp, fn, delta, vmax=vmin_max, processes=processes)
-
-        int_resp_list = box_table[:, 0]
-        vmin_center_list = box_table[:, 1]
-        vmin_error_left_list = box_table[:, 2]
-        vmin_error_right_list = box_table[:, 3]
+        
+        int_resp_list = box_table[0,:]
+        vmin_center_list = box_table[1,:]
+        vmin_error_left_list = box_table[2,:]
+        vmin_error_right_list = box_table[3,:]
         eta_list = self.BinData / int_resp_list
         eta_error_list = self.BinError / int_resp_list
         print('Bin Data', self.BinData)

src/experiment_HaloIndep_Band.py
@@ -596,14 +596,13 @@ class Experiment_EHI(Experiment_HaloIndep):
             optimum_log_likelihood = basinhopping(self.MultiExperimentMinusLogLikelihood, vars_guess,
                         minimizer_kwargs=minimizer_kwargs, niter=30, stepsize=.2)
         else:
-            optimum_log_likelihood = Custom_SelfConsistent_Minimization(class_name, vars_guess, mx, fp, fn, delta)[0]
-
+            optimum_log_likelihood, fun_val = Custom_SelfConsistent_Minimization(class_name, vars_guess, mx, fp, fn, delta)
 
         print(optimum_log_likelihood)
-        fun_val = (self._MinusLogLikelihood(optimum_log_likelihood) +
-                                            sum([class_name[y]._MinusLogLikelihood(optimum_log_likelihood,
-                                                 mx, fp, fn, delta)
-                                            for y in range(1, expernum)]))
+#        fun_val = (self._MinusLogLikelihood(optimum_log_likelihood) +
+#                                            sum([class_name[y]._MinusLogLikelihood(optimum_log_likelihood,
+#                                                 mx, fp, fn, delta)
+#                                            for y in range(1, expernum)]))
         print("MinusLogLikelihood =", fun_val)
         print("vars_guess =", repr(vars_guess))
         file = output_file_tail + "_GloballyOptimalLikelihood.dat"
@@ -1031,7 +1030,7 @@ class Experiment_EHI(Experiment_HaloIndep):
 
         if DEBUG:
             print(constr_optimum_log_likelihood[0])
-            print("optimum_logL =", self.optimal_logL)
+            print("Constrained optimum_logL =", constr_optimum_log_likelihood[1])
             print("vminStar_index =", vminStar_index)
 
         return constr_optimum_log_likelihood
@@ -1577,7 +1576,7 @@ class Experiment_EHI(Experiment_HaloIndep):
             x = table[:, 0]   # this is logeta
             y = table[:, 1]   # this is logL
             logL_interp = interpolate.interp1d(x, y, kind='cubic')
-
+            
             def _logL_interp(vars_list, constraints):
                 constr_not_valid = constraints(vars_list)[:-1] < 0
                 if np.any(constr_not_valid):
@@ -1617,17 +1616,24 @@ class Experiment_EHI(Experiment_HaloIndep):
                 plt.show()
 
             error = F
+            
             try:
                 if y[0] > self.optimal_logL + delta_logL and \
-                        logeta_minimLogL < self.optimal_logL + delta_logL:
+                        abs(logeta_minimLogL) < self.optimal_logL + delta_logL: 
                     sol = brentq(lambda logeta: logL_interp(logeta) - self.optimal_logL -
                                  delta_logL,
                                  table[0, 0], logeta_minimLogL)
+                    
                     self.vmin_logeta_band_low += \
                         [[self.vmin_sampling_list[index], sol]]
+                else:
+                    self.vmin_logeta_band_low += \
+                        [[self.vmin_sampling_list[index], -40.]]
             except ValueError:
                 print("ValueError: Error in calculating vmin_logeta_band_low")
                 error = T
+                
+                
             try:
                 if y[-1] > self.optimal_logL + delta_logL and \
                         logeta_minimLogL < self.optimal_logL + delta_logL:
@@ -1636,6 +1642,7 @@ class Experiment_EHI(Experiment_HaloIndep):
                                  logeta_minimLogL, table[-1, 0])
                     self.vmin_logeta_band_up += \
                         [[self.vmin_sampling_list[index], sol]]
+                    
             except ValueError:
                 print("ValueError: Error in calculating vmin_logeta_band_hi")
                 error = T

src/input_main.py
@@ -199,7 +199,7 @@ class Input:
                                           self.delta, mPhi=self.mPhi,
                                           quenching=self.quenching[0],
                                           EHI_METHOD=np.any(self.EHI_METHOD))
-                    if np.any(self.EHI_METHOD):
+                    if np.any(self.EHI_METHOD) and self.exper_name == "CDMSSi2012":
                         self.vmin_EHIBand_range = \
                             module.Vmin_EHIBand_range(self.exper_name.split()[0],
                                                       self.mx, self.delta, self.mPhi)

src/runprogram.py
@@ -271,7 +271,7 @@ class RunProgram:
         """Select which experiment class we must use, depending on what statistical
         analysis we need, and initialize the experiment.
         """
-
+        self.exper_name = exper_name
         print('name = ', exper_name)
         if HALO_DEP:
             print('Halo Dependent')
@@ -317,12 +317,13 @@ class RunProgram:
             # if delta > 0 we have to use the integration in recoil energy ER
             if delta > 0:
                 class_name.__bases__ = (Experiment_HaloIndep_ER,)
-
+        
         if exper_name in SHM_line:
             self.exper = class_name(exper_name, log_sigma_p)
         else:
             self.exper = class_name(exper_name, scattering_type, mPhi, quenching)
-
+        
+        
     def compute_data(self, mx, fp, fn, delta, mx_range, vmin_range, initial_energy_bin,
                      logeta_guess, HALO_DEP, MAKE_CROSSES, MAKE_LIMITS, EHI_METHOD,
                      vmin_EHIBand_range, logeta_EHIBand_percent_range, steepness,
@@ -341,7 +342,7 @@ class RunProgram:
                                                 output_file)
         else:
             (vmin_min, vmin_max, vmin_step) = vmin_range
-            if not np.any(EHI_METHOD):
+            if self.exper_name != "CDMSSi2012" or not np.any(EHI_METHOD):
                 if MAKE_LIMITS:
                     upper_limit = \
                         self.exper.UpperLimit(mx, fp, fn, delta, vmin_min, vmin_max,
@@ -354,7 +355,7 @@ class RunProgram:
                                               vmin_step, output_file,
                                               initial_energy_bin=initial_energy_bin)
 
-            else:
+            elif np.any(EHI_METHOD) and self.exper_name == "CDMSSi2012":
                 if EHI_METHOD.ResponseTables:
                     self.exper.ResponseTables(vmin_min, vmin_max, vmin_step, mx, fp, fn,
                                               delta, self.output_file_no_extension)
@@ -530,7 +531,7 @@ class RunProgram:
     def __call__(self, exper_name, scattering_type, mPhi, fp, fn, delta,
                  confidence_levels,
                  HALO_DEP, RUN_PROGRAM, MAKE_REGIONS, MAKE_CROSSES, MULTI_EXPER,
-                 MAKE_PLOT, EHI_METHOD, MAKE_LIMITS,
+                 MAKE_PLOT, EHI_METHOD, MAKE_LIMITS, multiexper_input=None,
                  mx=None, mx_range=None, vmin_range=None, initial_energy_bin=None,
                  vmin_EHIBand_range=None, logeta_EHIBand_percent_range=None,
                  steepness=None, logeta_guess=None,
@@ -698,13 +699,12 @@ class RunProgram_Multiexperiment:
         crosses = np.loadtxt(output_file)
         plot_data.plot_crosses(crosses, alpha=alpha, plot_show=False)
 
-    def plot_EHI_band(self, multiexper_input, class_name, confidence_levels, HALO_DEP, extra_tail,
-                      plot_dots):
-        output_file = MultiExper_Output_file_name(multiexper_input, scattering_type, mPhi, mx, fp, fn, delta,
-                     filename_tail, OUTPUT_MAIN_DIR, quenching=None)
-        class_name[0].ImportOptimalLikelihood(output_file)
+    def plot_EHI_band(self, multiexper_input, class_name, confidence_levels, HALO_DEP, extra_tail, output_file,
+                      output_file_CDMS, plot_dots):
+        
+        class_name[0].ImportMultiOptimalLikelihood(output_file,output_file_CDMS)
         interp_kind = 'linear'
-#        plot_limits = PlotData(multiexper_input[0], HALO_DEP, plot_close=False)
+        plot_limits = PlotData(multiexper_input[0], HALO_DEP, plot_close=False)
 
 #        self.exper.PlotSamplingTable(self.output_file_no_extension,
 #                                plot_close=False, plot_show=False, plot_optimum=False)
@@ -714,6 +714,8 @@ class RunProgram_Multiexperiment:
             PlotData.count[multiexper_input[0]] = -1
             class_name[0].ImportConfidenceBand(output_file, d_logL,
                                             extra_tail=extra_tail)
+            print('ddadadada')
+            print(class_name[0].vmin_logeta_band_low)
             first_vmin_low = class_name[0].vmin_logeta_band_low[0, 0]
             first_vmin_up = class_name[0].vmin_logeta_band_up[0, 0]
             last_eta_up = class_name[0].vmin_logeta_band_up[-1, 1]
@@ -730,15 +732,15 @@ class RunProgram_Multiexperiment:
             plot_limits(class_name[0].vmin_logeta_band_up,
                         lower_limit=class_name[0].vmin_logeta_band_low, kind=interp_kind,
                         fill=True, alpha=0.2, plot_dots=plot_dots, plot_show=False)
-
+        
         class_name[0].PlotOptimum(ylim_percentage=(1.2, 0.8),
-                               color=Color[exper_name + '_EHI'],
+                               color=Color[multiexper_input[0] + '_EHI'],
                                linewidth=3, plot_close=False,  plot_show=False)
 
     def __call__(self, multiexper_input, scattering_type, mPhi, fp, fn, delta,
                  confidence_levels,
                  HALO_DEP, RUN_PROGRAM, MAKE_REGIONS, MULTI_EXPER,
-                 MAKE_CROSSES, MAKE_PLOT, EHI_METHOD, MAKE_LIMITS,
+                 MAKE_CROSSES, MAKE_PLOT, EHI_METHOD, MAKE_LIMITS, exper_name=None,
                  mx=None, mx_range=None, vmin_range=None, initial_energy_bin=None,
                  vmin_EHIBand_range=None, logeta_EHIBand_percent_range=None,
                  steepness=None, logeta_guess=None,
@@ -882,7 +884,7 @@ class RunProgram_Multiexperiment:
 
             if EHI_METHOD.ConstrainedOptimalLikelihood:
                     # Tests for delta = 0:
-                    (vminStar, logetaStar) = (500, -27.0)
+                    (vminStar, logetaStar) = (25., -29.)
                     # Tests for delta = -50:
 #                    (vminStar, logetaStar) = (185.572266287, -19.16840262)
                     class_name[0].ImportMultiOptimalLikelihood(output_file, output_file_CDMS, plot=False)
@@ -940,7 +942,7 @@ class RunProgram_Multiexperiment:
                                                  extra_tail=extra_tail,
                                                  vmin_index_list=vmin_index_list,
                                                  logeta_index_range=logeta_index_range)
-            exit()
+            
             if EHI_METHOD.ConfidenceBand:
                     class_name[0].ImportMultiOptimalLikelihood(output_file, output_file_CDMS)
                     interpolation_order = 2
@@ -958,5 +960,5 @@ class RunProgram_Multiexperiment:
 
         # make band plot
         if EHI_METHOD.ConfidenceBandPlot:
-            self.plot_EHI_band(multiexper_input, class_name, confidence_levels, HALO_DEP, extra_tail,
-                               plot_dots)
+            self.plot_EHI_band(multiexper_input, class_name, confidence_levels, HALO_DEP, extra_tail, output_file,
+                               output_file_CDMS, plot_dots)