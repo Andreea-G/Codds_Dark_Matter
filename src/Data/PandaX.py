06a368e6646847fb513e2ad3f1beb1e88a68e771
Samuel Witte <witte.sam@gmail.com>
2017-02-25 15:34:40 -0800

    updated neg200 lim

--- src/Data/PandaX.py
+++ src/Data/PandaX.py
@@ -29,11 +29,12 @@ pi = np.pi
 name = "PandaX"
 modulated = False
 
-energy_resolution_type = "Poisson"
-#energy_resolution_type = "Gaussian"
+#energy_resolution_type = "Poisson"
+energy_resolution_type = "Gaussian"
+
 
 def EnergyResolution(e):
-    return np.ones_like(e)
+    return .1 * np.ones_like(e)
 
 FFSD = 'GaussianFFSD'
 FFSI = 'HelmFF'
@@ -55,21 +56,10 @@ target_nuclide_mass_list = np.array([115.418, 117.279, 119.141, 120.074, 121.004
 num_target_nuclides = target_nuclide_mass_list.size
 
 
-#QuenchingFactor = \
-#    interp1d(np.array([0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 40, 1000]),
-#             np.array([1., 1., 1., 1., 1., 1., 1.,
-#                       1., 1., 1., 1., 1., 1., 1., 1.]))
 
-x0 = np.array([0., 0.395837, 0.431065, 0.451241, 0.473339, 0.582546, 0.7747, 0.995997,
-               1.09303, 1.18078, 1.30024, 1.41681, 1.4783, 1.6948, 1.76781, 1.9004,
-               2.01954, 2.14444, 2.23187 ])
-y0 = np.array([0., 0.0745321, 0.0792141, 0.0831369, 0.0941459, 0.0999667, 0.116417,
-               0.144003, 0.147293, 0.154252, 0.159694, 0.169311, 0.17665, 0.191076,
-               0.194872, 0.198035, 0.197403, 0.2, 0.2])
-
-QuenchingFactor_interp = interp1d(x0, y0, kind='linear', bounds_error=False, fill_value=0.)
 
 def QuenchingFactor(e_list):
+    """
     Ly = 2.28
     Snr = 0.95
     See = 0.58
@@ -83,22 +73,31 @@ def QuenchingFactor(e_list):
                   else 0.2
                   for e in e_list])
     return Ly * Snr / See * q
+    """
+    try:
+        len(e_list)
+    except TypeError:
+        e_list = [e_list]
+    return np.ones_like(e_list)
 
 
-Ethreshold = 1.1
-Emaximum = 80.
-ERmaximum = 80.
-
-def Efficiency_ER(er):
-    return np.ones_like(er)
 
 
+Ethreshold = 0.
+Emaximum = 45.
+ERmaximum = 35.
 
 Eff_ER_interp = \
     interp1d(np.loadtxt('/Users/SamWitte/Desktop/Codds_DarkMatter/src/Data/eff_Pandax2016.dat')[:,0],
              np.loadtxt('/Users/SamWitte/Desktop/Codds_DarkMatter/src/Data/eff_Pandax2016.dat')[:,1],
              kind='linear', bounds_error=False,fill_value=0.)
 
+def Efficiency_ER(er):
+    try:
+        len(er)
+    except TypeError:
+        er = [er]
+    return np.array([Eff_ER_interp(e) if e > 1. and e < 35. else 0. for e in er])
 
 
 def Efficiency(e):
@@ -106,17 +105,17 @@ def Efficiency(e):
         len(e)
     except TypeError:
         e = [e]
-    return np.array([Eff_ER_interp(ee) if ee > 1.1 and ee < 70 else 0. for ee in e])
+    return np.array([1. if ee > 0. and ee < 40. else 0. for ee in e])
 #return Efficiency_interp(e) if 0.700414 <= e < 2.68015 \
 #   else np.array(0.) if e < 0.700414 else np.array(1.)
 
 Exposure = 3.3 * 10 ** 4.
-ERecoilList = np.array([25.5, 28., 30.5])
+ERecoilList = np.array([25., 28., 30.5])
 
 
 BinData = np.array([3.])
-BinEdges_left = np.array([2.0])
-BinEdges_right = np.array([80.0])
+BinEdges_left = np.array([1.1])
+BinEdges_right = np.array([35.])
 BinBkgr=np.array([4.8])
 BinSize=80.0
 BinExposure=np.array([Exposure])
